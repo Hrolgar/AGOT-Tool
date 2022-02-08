8e763c457872581c9c2f5588f3ee3104f0c73b8d
Helgi <helgiskafta@gmail.com>
2022-02-08 15:21:20 +0100

    Changed file name for province_detail.

--- GenerateFiles/GenerateProvinceTerrain.cs
+++ GenerateFiles/GenerateProvinceTerrain.cs
@@ -8,7 +8,7 @@ public class GenerateProvinceTerrain : GenerateClasses
     public void Generate(string generatedFile)
     {
         var folderStruct = Directory.CreateDirectory(@$"{generatedFile}\common\province_terrain\");
-        var fileName = @$"{folderStruct}\province_terrain.txt";
+        var fileName = @$"{folderStruct}\00_province_terrain.txt";
         var writer = new StreamWriter(fileName, false, Encoding.Default);
         var txt = "default=plains\n";
         foreach (var eKingdom in Empires.SelectMany(empire => empire.Kingdoms))
